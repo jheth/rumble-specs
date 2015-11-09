e7cf0c022912c40750279c349c9a36e28619a168
Clayton Parker <me@claytron.com>
2015-11-09 12:38:54 -0500

    fix rename of method in tests

--- spec/lib/rumble/condition_spec.rb
+++ spec/lib/rumble/condition_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Rumble::Condition do
   let(:subject) { Rumble::Condition.new }
 
   it 'works' do
-    expect(subject.has_access(45)).to eq(true)
+    expect(subject.access?(45)).to eq(true)
   end
 
   it 'does not work' do
-    expect(subject.has_access(89)).to eq(false)
+    expect(subject.access?(89)).to eq(false)
   end
 end
